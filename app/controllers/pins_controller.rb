01a5c6bbd42e6adaf604c7e436f5f1e113b48156
bhushan <morapost@gmail.com>
2016-12-06 19:13:32 +0530

    Pin CRUD ability

---
+++ app/controllers/pins_controller.rb
@@ -0,0 +1,46 @@
+class PinsController < ApplicationController
+	before_action :find_pin, except: [:index,:new, :create]
+
+	def index
+		@pins = Pin.all.order('created_at DESC')
+	end
+
+	def new
+		@pin = Pin.new
+	end
+
+	def create
+		@pin = Pin.new(pin_params)
+		if @pin.save
+			redirect_to @pin, notice: "Successfully Created a Pin"
+		else
+			render 'new'
+		end
+	end
+
+	def edit
+		
+	end
+
+	def update
+		if @pin.update(pin_params)
+			redirect_to @pin, notice: "Successfully Updated a Pin"
+		else
+			render 'edit'
+		end
+	end
+
+	def destroy
+		@pin.destroy
+		redirect_to root_path
+	end
+
+	private
+	def pin_params
+		params.require(:pin).permit(:title, :description)
+	end
+
+	def find_pin
+		@pin = Pin.find(params[:id])
+	end
+end
